d23a792999da0c2d2d4243e5f98815cc57a18559
D8GER <xyb5to0zcy@gmail.com>
2025-06-20 18:19:32 +0800

    fix(sitemap): regrex repair!

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,7 @@ export default function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-      // Skip Next.js internals and all static files, unless found in search params, skip api and trpc
-      '/((?!api|trpc|_next|[^?]*.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|otf)).*)',
+    // Skip Next.js internals, static files, sitemap, robots, unless found in search params, skip api and trpc
+    '/((?!api|trpc|_next|sitemap.xml?|robots.txt?|[^?]*.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|otf)).*)',
   ],
 };
