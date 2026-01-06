eab642f205b80b63c6e397627d5170387c1dfecf
D8GER <xyb5to0zcy@gmail.com>
2026-01-06 23:17:49 +0800

    fix(newspaper): update font configurations and layout adjustments for song components; modify image paths for better asset management

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -24,7 +24,7 @@ export const appConfig = {
       name: "Chinese Song Poster",
       type: "song",
       key: "song_cn",
-      thumb: "https://r2.d8ger.com/newspaper-template/modern.webp",
+      thumb: "/song_cn.webp",
       href: "",
       top: false
     },
@@ -32,7 +32,7 @@ export const appConfig = {
       name: "English Song Poster",
       type: "song",
       key: "song_en",
-      thumb: "https://r2.d8ger.com/newspaper-template/modern.webp",
+      thumb: "/song_en.webp",
       href: "",
       top: false
     }
